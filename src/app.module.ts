df67798e35ddb8218b4553efbaa8c081746689d1
Moacir Amaro <moacirjgt@gmail.com>
2023-01-19 22:17:26 -0300

    feat: add typeorm lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TariffModule } from './tariff/tariff.module';
 import { PlanModule } from './plan/plan.module';
 import { CallModule } from './call/call.module';
+import { TypeOrmConfigModule } from './typeorm-database.module';
 
 @Module({
-  imports: [CallModule, TariffModule, PlanModule],
+  imports: [CallModule, TariffModule, PlanModule, TypeOrmConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/migration.config.ts
@@ -0,0 +1,11 @@
+import { typeOrmConfig } from './typeorm.config'
+import { DataSource } from "typeorm"
+
+//@ts-expect-error
+export default new DataSource({
+	
+	...typeOrmConfig,
+	entities: ['src/**/entities/*.entity.ts'],
+	migrations: ['db/migrations/*.ts'],
+	migrationsTransactionMode: 'each',
+  })
