443c4e8b6062ea5c62bccba5eaa3b6213d3dbc88
Moacir Amaro <moacirjgt@gmail.com>
2023-01-22 18:05:48 -0300

    feat: add tariff repository provider

--- src/tariff/domain/ports/tariff.repository.provider.ts
+++ src/tariff/domain/ports/tariff.repository.provider.ts
@@ -2,7 +2,7 @@ import { ClassProvider } from "@nestjs/common";
 import { TypeOrmTariffRepository } from "../../adapter/db/typeorm/repositories/typeorm-tariff.repository";
 import { TariffRepository } from "./tariff.repository";
 
-export class TariffRepositoryProvider implements ClassProvider<TariffRepository> {
-	provide = TariffRepository
-	useClass = TypeOrmTariffRepository
+export const  TariffRepositoryProvider: ClassProvider = {
+	provide: TariffRepository,
+	useClass: TypeOrmTariffRepository
 }
--- src/tariff/tariff.module.ts
+++ src/tariff/tariff.module.ts
@@ -4,14 +4,12 @@ import { TariffController } from './adapter/api/tariff.controller';
 import { TypeOrmTariff } from './adapter/db/typeorm/entities/typeorm-tariff.entity';
 import { TypeOrmTariffRepository } from './adapter/db/typeorm/repositories/typeorm-tariff.repository';
 import { TariffRepository } from './domain/ports/tariff.repository';
+import { TariffRepositoryProvider } from './domain/ports/tariff.repository.provider';
 
 @Module({
   controllers: [TariffController],
   imports: [TypeOrmModule.forFeature([TypeOrmTariff])],
-  providers: [{
-	provide: TariffRepository,
-	useClass: TypeOrmTariffRepository
-  }],
-  exports: [TariffRepository]
+  providers: [TariffRepositoryProvider],
+  exports: [TariffRepositoryProvider]
 })
 export class TariffModule {}
