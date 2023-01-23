6039daf8156982a1e37ff7e86df7d2fe69a223dd
Moacir Amaro <moacirjgt@gmail.com>
2023-01-23 20:46:12 -0300

    refactor: rename private method

--- src/call/domain/model/value-objects/call-duration-in-minutes.vo.ts
+++ src/call/domain/model/value-objects/call-duration-in-minutes.vo.ts
@@ -4,12 +4,12 @@ export class CallDurationInMinutes {
   private constructor(public readonly value: number) {}
 
   static create(value: number) {
-    this.isValidPlanName(value);
+    this.isValidDuration(value);
 
     return new CallDurationInMinutes(value);
   }
 
-  private static isValidPlanName(value: number) {
+  private static isValidDuration(value: number) {
     if (!value || value < 1) {
       throw new BadRequestException('O tempo de ligação deve ser maior que 0');
     }
